167abd69a2cf71dc62ca47f9be57879467bae072
lang thanh <langvietthanh.28072005@gmail.com>
2026-02-10 16:51:44 +0700

    Xóa các dòng console dùng để debug

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,26 +7,14 @@ const { passwordChecker } = require('../utils/security');
 class authService {
     async registerUser(userData){
         const {username, email, password, height, weight, gender, birthDate, activityLevel, goal, targetWeight} = userData;
-        
-//      Kiem tra email ton tai chua
-        console.log(`🔍 Đang tìm kiếm: ${email}`);
 
-        // Đảm bảo là findOne
+//      Kiem tra su ton tai cua Email
         const existingEmail = await User.findOne({ email }); 
-
-        // --- DÒNG QUAN TRỌNG NHẤT: IN RA XEM NÓ LÀ CÁI QUÁI GÌ ---
-        console.log('👉 GIÁ TRỊ EXISTING USER LÀ:', existingEmail); 
-        console.log('👉 KIỂU DỮ LIỆU:', typeof existingEmail);
-        console.log('👉 CÓ PHẢI MẢNG KHÔNG:', Array.isArray(existingEmail));
-
         if (existingEmail) {
-        console.log('❌ Code đang nhảy vào IF (nghĩa là nó coi existingUser là True)');
         throw new Error('Email đã tồn tại');
-        } else {
-        console.log('✅ Code đã bỏ qua IF (nghĩa là existingUser là Null)');
         }
 
-//      Kiem tra User ton tai chua
+//      Kiem tra su ton tai cua User
         const existingUsername = await User.findOne({ username }); // Nhớ dùng findOne
         if (existingUsername) {
             throw new Error('Tên đăng nhập đã tồn tại');
@@ -68,20 +56,6 @@ class authService {
 
 //      Tao JWT de login luon -- doc them document/New_Knowledge 
         const payload = { userId: newUser._id };
-
-// ...
-console.log('--- BẮT ĐẦU DEBUG ENV ---');
-
-// 1. In ra tất cả các Key có chữ JWT để xem bạn có gõ nhầm tên không
-const allKeys = Object.keys(process.env);
-const jwtKeys = allKeys.filter(key => key.includes('JWT') || key.includes('SECRET'));
-console.log('👉 Các biến tìm thấy trong ENV:', jwtKeys);
-
-// 2. In thử giá trị (Nếu key trên kia in ra khác, bạn sửa lại theo nó)
-console.log('👉 Giá trị chính xác:', process.env[jwtKeys[0]]); 
-
-console.log('--- KẾT THÚC DEBUG ---');
-
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '7d' });
 
         return { token, user: newUser };
